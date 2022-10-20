135970a060976762765877dd09cc15626fd5a66e
Roshish Shrestha <roshish152002@gmail.com>
2022-10-20 22:06:41 +0545

    50: Pow(x, n)

---
+++ my_pow.dart
@@ -0,0 +1,12 @@
+class Solution {
+  double myPow(double x, int n) {
+    if (n == 0) return 1;
+    if (n < 0) return 1 / x * myPow(1 / x, -(n + 1));
+    return n % 2 == 0 ? myPow(x * x, n ~/ 2) : x * myPow(x * x, n ~/ 2);
+  }
+}
+
+void main() {
+  Solution sol = Solution();
+  print(sol.myPow(2, -2));
+}
