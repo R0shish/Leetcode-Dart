16fa4f4c80c2a8d8b4818f1f1dcba1f935145b76
Roshish Shrestha <roshish152002@gmail.com>
2022-10-20 21:27:05 +0545

    1: Two Sum

--- two_sum.dart
+++ two_sum.dart
@@ -1,3 +1,17 @@
 class Solution {
-  List<int> twoSum(List<int> nums, int target) {}
+  List<int> twoSum(List<int> nums, int target) {
+    final Map<int, int> map = {};
+    for (int i = 0; i < nums.length; i++, map[nums[i]] = i) {
+      final int diff = target - nums[i];
+      if (map.containsKey(diff)) {
+        return [map[diff]!, i];
+      }
+    }
+    return [];
+  }
+}
+
+void main() {
+  Solution sol = Solution();
+  print(sol.twoSum([2, 1, 3, 4], 4));
 }
