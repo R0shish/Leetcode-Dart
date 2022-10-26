1b389322ae3d91c28124de4a050a0795a34f9c54
Roshish Shrestha <roshish152002@gmail.com>
2022-10-26 16:52:32 +0545

    21: Merge Two Sorted Lists

---
+++ merge_two_sorted_list.dart
@@ -0,0 +1,20 @@
+// Definition for singly-linked list.
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
+class Solution {
+  ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+
+    if (list1.val < list2.val) {
+      list1.next = mergeTwoLists(list1.next, list2);
+      return list1;
+    }
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
+  }
+}
